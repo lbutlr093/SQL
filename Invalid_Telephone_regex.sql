e7a3f59eb1a36cace7cc6d3b1f6a80ac405926a9
lbutlr093 <lbutlr093@gmail.com>
2016-09-30 09:30:02 -0500

    Update Invalid_Telephone_regex.sql

--- Invalid_Telephone_regex.sql
+++ Invalid_Telephone_regex.sql
@@ -4,10 +4,12 @@ SELECT PHONE_NUMBER
 FROM TABLESPACE.TABLENAME
 WHERE SPECIFIC_CODE = 1
 AND NOT REGEXP_LIKE ( PHONE_NUMBER, '^\([2-9]{1}[0-9]{2}\)[\ |\-|\.]?[2-9]{1}[0-9]{2}[\ |\-|\.]?[0-9]{4,}$|^[2-9]{1}[0-9]{2}[\ |\-|\.]?[2-9]{1}[0-9]{2}[\ |\-|\.]?[0-9]{4,}$')
-
+--10+ digits
+  
 UNION
 
 SELECT PHONE_NUMBER
 FROM TABLESPACE.TABLENAME
 WHERE SPECIFIC_CODE = 1
 AND REGEXP_LIKE ( PHONE_NUMBER, '^\([2-9]{1}[0-9]{2}\)[\ |\-|\.]?[5]{3}[\ |\-|\.]?[0-9]{4,}$|^[2-9]{1}[0-9]{2}[\ |\-|\.]?[5]{3}[\ |\-|\.]?[0-9]{4,}$') );
+--exchange of '555'
